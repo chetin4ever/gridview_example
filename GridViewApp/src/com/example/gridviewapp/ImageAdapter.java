014ef3cf3f2dab69cd5711db34e125b81fc13197
chetin4ever <hetan009@gmail.com>
2014-12-24 15:39:48 +0530

    image display sucessfully

--- GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
+++ GridViewApp/src/com/example/gridviewapp/ImageAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
 	
@@ -16,7 +18,7 @@ public class ImageAdapter extends BaseAdapter {
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return mThumbIds.length;
 	}
 
 	@Override
@@ -32,9 +34,22 @@ public class ImageAdapter extends BaseAdapter {
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageView;
+		if(convertView==null){
+			imageView = new ImageView(mContext);
+			imageView.setLayoutParams(new GridView.LayoutParams(150,150));
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setPadding(5, 5, 5, 5);
+			
+			
+		}else{
+			imageView=(ImageView) convertView;
+		}
+		imageView.setImageResource(mThumbIds[position]);
+		
+		
+		return imageView;
 	}
 	public Integer[] mThumbIds ={
 			R.drawable.sample_0, R.drawable.sample_0,
