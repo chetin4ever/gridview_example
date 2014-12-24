a631eb82aac2ca13d22d0930e191f862cb69220c
chetin4ever <hetan009@gmail.com>
2014-12-24 16:21:31 +0530

    version one of gallery complete

--- GridViewApp/src/com/example/gridviewapp/SingleActivity.java
+++ GridViewApp/src/com/example/gridviewapp/SingleActivity.java
@@ -1,8 +1,11 @@
 package com.example.gridviewapp;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class SingleActivity extends Activity {
 
@@ -10,6 +13,12 @@ public class SingleActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_single);
+		
+		Intent i = getIntent();
+		int position = i.getExtras().getInt("id");
+		ImageAdapter imageAdapter = new ImageAdapter(SingleActivity.this);
+		ImageView imageView =(ImageView) findViewById(R.id.imageView1);
+		imageView.setImageResource(imageAdapter.mThumbIds[position]);
 	}
 
 	@Override
